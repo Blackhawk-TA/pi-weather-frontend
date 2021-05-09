8fb6b0ba56e1a09866bdc7ff9316db507472e7aa
Blackhawk <daniel.peters1@pm.me>
2021-05-09 03:19:22 +0200

    Build

---
+++ build/precache-manifest.3e9078164b3714cad2dfffe9bcc81d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b24bee8c7acfe01ead416f3dda9eba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351233e3c5547a7c3b56",
+    "url": "/static/css/main.e76011c8.chunk.css"
+  },
+  {
+    "revision": "176830a5fdc651412ad9",
+    "url": "/static/js/2.9ca323af.chunk.js"
+  },
+  {
+    "revision": "de1d0b6baa938b4a49e6f2ae55c4bbcc",
+    "url": "/static/js/2.9ca323af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "351233e3c5547a7c3b56",
+    "url": "/static/js/main.49da341b.chunk.js"
+  },
+  {
+    "revision": "c9144ef7f366943317dc",
+    "url": "/static/js/runtime-main.c4b56517.js"
+  }
+]);
